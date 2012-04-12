6cc8ddc8c4b6ed2aec2cd4d41efac982a4f961d0
CookieDev <dev@casual4.de>
2012-04-12 11:44:41 +0200

    init

---
+++ core/cookieapp.php
@@ -0,0 +1,29 @@
+<?php
+
+	/*
+	 * Hauptklasse des Cookie-Frameworks
+	 * 
+	 * @version 11/04/2012
+	 * @author Benjamin Abicht / Stefan Schaetz
+	 */
+	class CookieApp {
+
+		/*
+         * Konstruktor
+		 */
+		function __construct() {
+       		//
+  		}
+
+  		/*
+  		 * Destruktor
+  		 */
+		function __destruct() {
+        	//
+   		}
+   		
+	}
+
+
+
+?>
